432de1b960cdee9ad7ab688c111b43464526f1c2
hor1zon <romashov_e@mail.ru>
2021-04-21 23:54:30 +0300

    task #40 added

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// 40. Даны картинки. Привяжите к каждой картинке событие, чтобы по клику на картинку алертом выводился ее src.
+
+let images = document.querySelectorAll("img");
+
+// images.forEach(addEventListener("click", (e) => {
+//     let attr = e.target.getAttribute("src");
+//     alert(attr);
+// }));
+
+images.forEach( img => {
+    img.addEventListener("click", (e) => {
+        let attr = e.target.getAttribute("src");
+        alert(attr);
+    })
+})
+
+
+// 42. В переменной min лежит число от 0 до 59. Определите в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+
+
+
+// 45. Дана строка 'aaa bbb ccc'. Вырежите из нее слово 'bbb' тремя разными способами (через substr, substring, slice).
+
+// let string = "aaa bbb ccc";
+
+// console.log(string.slice(4, 7));
+// console.log(string.substr(4, 3));
+// console.log(string.substring(4, 7));
+
